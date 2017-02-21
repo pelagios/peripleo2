f71bf9970a13978f085548743b9687e575f47cfc
Rainer Simon <rainer.simon@ait.ac.at>
2017-02-21 17:25:33 +0100

    Some tinkering re issue #19

--- app/services/item/search/SearchService.scala
+++ app/services/item/search/SearchService.scala
@@ -7,7 +7,7 @@ import play.api.libs.json.Json
 import scala.concurrent.{ Future, ExecutionContext }
 import services.{ ES, Page }
 import services.item.Item
-  
+
 @Singleton
 class SearchService @Inject() (val es: ES, implicit val ctx: ExecutionContext) {
 
@@ -15,20 +15,37 @@ class SearchService @Inject() (val es: ES, implicit val ctx: ExecutionContext) {
     override def as(hit: RichSearchHit): Item =
       Json.fromJson[Item](Json.parse(hit.sourceAsString)).get
   }
-  
+
   def query(args: SearchArgs): Future[Page[Item]] = {
-    es.client execute {
+    val searchDefinition =
       args.query match {
-        case Some(query) =>
-          search in ES.PERIPLEO / ES.ITEM query query start args.offset limit args.limit
-          
-        case None =>
-          search in ES.PERIPLEO / ES.ITEM start args.offset limit args.limit
+        case Some(q) => search in ES.PERIPLEO / ES.ITEM query q
+        case None => search in ES.PERIPLEO / ES.ITEM
       }
+
+    es.client execute {
+      searchDefinition start args.offset limit args.limit aggregations (
+        aggregation
+          terms "by_type"
+          field "item_type"
+          size 20,
+
+        aggregation
+          histogram "by_year"
+          interval 10
+          
+          // TODO this now generates matches at 1970 (0 in UNIX time) for items without temporal_bounds
+          
+          script "from = doc['temporal_bounds.from'].date.year; to = doc['temporal_bounds.to'].date.year; (from..to)"
+      )
     } map { response =>
-      val items = response.as[Item].toSeq 
-      Page(response.tookInMillis, response.totalHits, args.offset, args.limit, items)  
-    }     
+
+      // TODO parse & wrap aggregation results
+      play.api.Logger.info(response.toString)
+
+      val items = response.as[Item].toSeq
+      Page(response.tookInMillis, response.totalHits, args.offset, args.limit, items)
+    }
   }
-  
+
 }
