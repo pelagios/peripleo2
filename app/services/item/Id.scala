88cdc11ec84689908db0d7524b2d493b0b50437f
Rainer Simon <rainer.simon@ait.ac.at>
2017-04-01 11:01:19 +0200

    Minor edit

--- app/services/item/Id.scala
+++ app/services/item/Id.scala
@@ -5,3 +5,10 @@ sealed trait Id
 case object UndefinedId extends Id
 
 case class DefindedId(value: String) extends Id
+
+object Id {
+  
+  // For readability when creating Items/References
+  val AUTOGENERATE = UndefinedId
+  
+}
