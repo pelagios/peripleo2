eae46e37296c97f7b2041bc03140341bad79a7a9
Rainer Simon <rainer.simon@ait.ac.at>
2017-04-19 10:24:20 +0200

    Minor fix re PathHierarchy model change

--- app/assets/javascripts/ui/common/itemUtils.js
+++ app/assets/javascripts/ui/common/itemUtils.js
@@ -3,8 +3,9 @@ define(function() {
   return {
 
     /** We'll do this pre-processing step on the server later! **/
-    getHierarchyPath : function(path) {
-      var last = path[path.length - 1],
+    getHierarchyPath : function(hierarchy) {
+      var path = hierarchy.paths,
+          last = path[path.length - 1],
           tuples = last.split('\u0007\u0007');
 
       return tuples.map(function(str) {
@@ -16,7 +17,7 @@ define(function() {
     /** Shorthand to make handling of item types a bit easier **/
     getItemType : function(item) {
       var t = item.item_type;
-      
+
       if (t.indexOf('PLACE') > -1) {
         return 'PLACE';
       } else if (t.indexOf('OBJECT') > -1) {
