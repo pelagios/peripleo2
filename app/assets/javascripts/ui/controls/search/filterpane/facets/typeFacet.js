465094724e548485b91c59d38a15cf050bf78c2f
Rainer Simon <rainer.simon@ait.ac.at>
2017-05-29 17:00:15 +0200

    Base plumbing for type facet widget

--- app/assets/javascripts/ui/controls/search/filterpane/facets/typeFacet.js
+++ app/assets/javascripts/ui/controls/search/filterpane/facets/typeFacet.js
@@ -2,98 +2,63 @@ define([
   'ui/common/formatting'
 ], function(Formatting) {
 
+  var ICONS = {
+        PLACE   : '&#xf041;',
+        OBJECT  : '&#xf219;',
+        PERSON  : '&#xf007;',
+        DATASET : '&#xf187;'
+      },
+
+      MIN_PCNT = 27;
+
   var TypeFacet = function(parentEl) {
 
     var el = jQuery(
           '<div class="facet by-type left">' +
-            '<ul>' +
-              '<li>' +
-                '<div class="count object">' +
-                  '<span class="icon">&#xf219;</span><span class="label">1,201</span>' +
-                '</div>' +
-              '</li>' +
-
-              '<li style="width:65%">' +
-                '<div class="count place">' +
-                  '<span class="icon">&#xf041;</span><span class="label">548</span>' +
-                '</div>' +
-              '</li>' +
-
-              '<li style="width:35%">' +
-                '<div class="count person">' +
-                  '<span class="icon">&#xf007;</span><span class="label">12</span>' +
-                '</div>' +
-              '</li>' +
-            '</ul>' +
-            //'<div class="more-buckets">More...</div>' +
+            '<ul></ul>' +
           '</div>').appendTo(parentEl),
 
-        update = function(buckets) {
-          /*
-          var total = buckets.reduce(function(acc, bucket) {
-                var count = bucket[Object.keys(bucket)[0]];
-                return acc + count;
-              }, 0),
+        template =
+          '<li>' +
+            '<div class="facet-bar"><span class="icon"></span><span class="label"></span></div>' +
+          '</li>',
 
-              topThree = buckets.slice(0, 3).map(function(bucket) {
-                var t = Object.keys(bucket)[0],
-                    v = bucket[t];
+        list = el.find('ul'),
 
-                return { type: t, count: v };
+        update = function(arr) {
+          var buckets = arr.map(function(obj) {
+                var key = Object.keys(obj)[0],
+                    val = obj[key];
+
+                return { name: key, count: val };
               }),
 
-              bottomHeight = (topThree.length < 2) ? 0 : topThree.slice(1,3).reduce(function(acc, b) {
-                return acc + b.count;
-              }, 0) / total,
+              total = buckets.reduce(function(acc, bucket) {
+                return acc + bucket.count;
+              }, 0),
 
-              topHeight = 1 - bottomHeight,
+              getWidth = function(val) {
+                var pcnt = Math.round(100 * val / total);
+                return Math.max(pcnt, MIN_PCNT) + '%';
+              },
 
-              append = function(facet, width, height, minW, maxW, minH, maxH, buffer) {
-                var span = jQuery('<span class="type-count"></span>'),
+              render = function(bucket) {
+                var li = jQuery(template),
+                    icon = li.find('.icon'),
+                    label = li.find('.label');
 
-                    toPcnt = function(n, min, max) {
-                      var pcnt = Math.round(100 * n) - buffer;
-                      return Math.min(Math.max(min, pcnt), max) + '%';
-                    };
+                li.addClass(bucket.name);
+                li.css('width', getWidth(bucket.count));
 
-                span.addClass(facet.type);
-                span.html(Formatting.formatNumber(facet.count));
-                span.css({
-                  width: toPcnt(width, minW, maxW),
-                  height: toPcnt(height, minH, maxH)
-                });
+                icon.html(ICONS[bucket.name]);
 
-                el.append(span);
+                label.html(Formatting.formatNumber(bucket.count));
 
-                // Set fontsize once we know absolute dimensions
-                span.css({
-                  lineHeight: span.outerHeight() + 'px',
-                  fontSize: Math.round(0.7 * span.outerHeight())+ 'px'
-                });
+                list.append(li);
               };
 
-          el.empty();
-
-          if (topThree.length > 0) {
-            // First line - 1st facet count
-            if (topThree.length === 1)
-              // Only a single facet count
-              append(topThree[0], 1, topHeight, 0, 100, 0, 100, 0);
-            else
-              // At least two facet counts
-              append(topThree[0], 1, topHeight, 0, 100, 35, 65, 1.5);
-
-            // Second line - 2nd and 3rd facet count
-            if (topThree.length == 2) {
-              // Two facet counts
-              append(topThree[1], 1, bottomHeight, 0, 100, 35, 65, 1.5);
-            } else if (topThree.length === 3) {
-              // Three facet counts
-              var w = topThree[1].count / (topThree[1].count + topThree[2].count);
-              append(topThree[1], w, bottomHeight, 20, 80, 35, 65, 1.5);
-              append(topThree[2], 1 - w, bottomHeight, 20, 80, 35, 65, 1.5);
-            }
-          } */
+          list.empty();
+          buckets.slice(0, 3).forEach(render);
         };
 
     this.update = update;
