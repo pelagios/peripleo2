b964c0e560789fecdcba0e6fcb37d477bc27f2b9
Rainer Simon <rainer.simon@ait.ac.at>
2017-05-29 14:46:13 +0200

    Starting to wire functionality into stats display

--- app/assets/javascripts/ui/controls/search/filterpane/facets/sourceFacet.js
+++ app/assets/javascripts/ui/controls/search/filterpane/facets/sourceFacet.js
@@ -6,15 +6,28 @@ define([
 
     var el = jQuery(
           '<div class="facet by-source right">' +
-            '<div class="top-count">1,802</div>' +
-            '<div class="top-source">' +
-              'American Numismatic Society' +
-            '</div>' +
-            '<div class="more-buckets"><span class="icon">&#xf187;</span> 5 other sources</div>' +
+            '<div class="top-count"></div>' +
+            '<div class="top-source"></div>' +
+            '<div class="more-buckets"></div>' + // '<span class="icon">&#xf187;</span> 5 other sources</div>' +
           '</div>').appendTo(parentEl),
 
+        topCount = el.find('.top-count'),
+
+        topSource = el.find('.top-source'),
+
         update = function(buckets) {
-          console.log(buckets);
+          var top = buckets[0],
+
+              source = Object.keys(top)[0],
+              sourcePaths = source.split('\u0007\u0007'),
+              longestPath = sourcePaths[sourcePaths.length - 1].split('\u0007'),
+
+              count = top[source];
+
+          console.log(longestPath[1]);
+
+          topCount.html(Formatting.formatNumber(count));
+          topSource.html(longestPath[1]);
         };
 
     this.update = update;
