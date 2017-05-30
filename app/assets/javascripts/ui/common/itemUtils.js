1e537f7da6f3cd1d33681df305818e0f4d399485
Rainer Simon <rainer.simon@ait.ac.at>
2017-05-30 11:20:07 +0200

    Vici.org style

--- app/assets/javascripts/ui/common/itemUtils.js
+++ app/assets/javascripts/ui/common/itemUtils.js
@@ -5,6 +5,7 @@ define(function() {
         // Places
         { shortcode: 'pleiades',    initial: 'P', color: '#5b9ec4', url_patterns: [ 'http://pleiades.stoa.org/places/' ] },
         { shortcode: 'dare',        initial: 'D', color: '#9e9ac8', url_patterns: [ 'http://dare.ht.lu.se/places/' ] },
+        { shortcode: 'vici',        initial: 'V', color: '#ff9896', url_patterns: [ 'http://vici.org/vici/' ] },
         { shortcode: 'geonames',    initial: 'G', color: '#74c476', url_patterns: [ 'http://sws.geonames.org/' ] },
         { shortcode: 'opencontext', initial: 'O', color: '#8c6d31', url_patterns: [ 'http://opencontext.org/subjects/' ]},
 
