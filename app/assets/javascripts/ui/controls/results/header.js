dc07feeb5ea373faa49f27600bcf4217b1497629
Rainer Simon <rainer.simon@ait.ac.at>
2017-06-13 16:37:59 +0200

    More filter bar styling (issue #136)

--- app/assets/javascripts/ui/controls/results/header.js
+++ app/assets/javascripts/ui/controls/results/header.js
@@ -7,18 +7,36 @@ define([], function() {
     var el = jQuery(
           '<div class="rl-h-container">' +
             '<div class="rl-h-label icon">&#xf0b0;</div>' +
-            '<div class="rl-h-filters"></div>' +
+            '<ul class="rl-h-filters"></ul>' +
             '<div class="rl-h-clear"><span class="icon stroke7">&#xe680;</span></div>' +
-          '</div>').appendTo(parentEl);
+          '</div>').appendTo(parentEl),
+
+        filterList = el.find('.rl-h-filters'),
 
         hide = function() {
           if (parentEl.is(':visible'))
             parentEl.velocity('slideUp', { duration: SLIDE_DURATION });
         };
 
-    // TODO
-    this.hide = function() {};
+    /* Dummy - just to test the design
+    filterList.append(
+      '<li class="collapsed">' +
+        '<span class="icon">&#xf041;</span>' +
+      '</li>');
 
+      filterList.append(
+        '<li>' +
+          '<span class="icon">&#xf187;</span>' +
+          '<a class="label destination" href="#">University of Graz</a>' +
+        '</li>');
+
+      filterList.append(
+        '<li class="collapsed">' +
+          '<span class="icon">&#xf017;</span>' +
+        '</li>');
+      */
+
+    this.hide = hide;
   };
 
   return Header;
