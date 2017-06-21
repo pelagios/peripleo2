ab5553f4baf523cf943eab6c018a7965b5f1e884
Rainer Simon <rainer.simon@ait.ac.at>
2017-06-21 09:33:45 +0200

    Closes #77

--- app/assets/javascripts/ui/controls/selection/cards/periodCard.js
+++ app/assets/javascripts/ui/controls/selection/cards/periodCard.js
@@ -1,23 +1,36 @@
 define([
-  'ui/common/formatting'
-], function(Formatting) {
+  'ui/common/formatting',
+  'ui/common/itemUtils'
+], function(Formatting, ItemUtils) {
 
   var PeriodCard  = function(parentEl, period, args) {
 
     var infoEl = jQuery(
           '<div class="item-info">' +
+            '<p class="item-is-in"></p>' +
             '<h3 class="item-title"></h3>' +
             '<p class="item-homepage"></p>' +
             '<p class="item-temporal-bounds"></p>' +
           '</div>').appendTo(parentEl),
 
+          inDatasetEl  = infoEl.find('.item-is-in'),
           titleEl      = infoEl.find('.item-title'),
           homepageEl   = infoEl.find('.item-homepage'),
           tempBoundsEl = infoEl.find('.item-temporal-bounds'),
 
           render = function() {
+            // For the time being, periods only come from PeriodO, so one record only
+            var record = period.is_conflation_of[0];
+
+            ItemUtils.getHierarchyPath(record.is_in_dataset).forEach(function(segment) {
+              inDatasetEl.append(
+                '<span><a class="destination" data-id="' + segment.id + '" href="#">' +
+                  segment.title +
+                '</a></span>');
+            });
+
             titleEl.html(period.title);
-            homepageEl.html(period.is_conflation_of[0].uri);
+            homepageEl.html(record.uri);
 
             if (period.temporal_bounds)
               tempBoundsEl.html(Formatting.formatTemporalBounds(period.temporal_bounds));
