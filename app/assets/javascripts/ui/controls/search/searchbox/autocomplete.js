ec6d2d80def3be8653b657b16db86af682f7576d
Rainer Simon <rainer.simon@ait.ac.at>
2017-07-11 12:45:10 +0200

    Cleaned up autocomplete.js

--- app/assets/javascripts/ui/controls/search/searchbox/autocomplete.js
+++ app/assets/javascripts/ui/controls/search/searchbox/autocomplete.js
@@ -4,22 +4,23 @@ define([
 ], function(HasEvents, API) {
 
   var Autocomplete = function(form, input) {
+
     var self = this;
 
+    // Typeahead setup
     input.typeahead({
-      hint: false,
-      highlight: false,
-      minLength: 1
+      hint      : false,
+      highlight : false,
+      minLength : 1
     },{
-      async: true,
-      limit: 42, // Behold: https://github.com/twitter/typeahead.js/issues/1312
-      display: function(obj) { return obj.text; },
-      templates: {
+      async     : true,
+      limit     : 42, // Behold: https://github.com/twitter/typeahead.js/issues/1312
+      display   : function(obj) { return obj.text; },
+
+      templates : {
+
         suggestion: function(data) {
-          var element = jQuery(
-            '<div>' +
-              '<span class="label">' + data.text + '</span>' +
-            '</div>');
+          var element = jQuery('<div><span class="label">' + data.text + '</span></div>');
 
           if (data.item_type) {
             element.addClass('entity ' + data.item_type[0]);
@@ -31,8 +32,10 @@ define([
 
           return element;
         }
+
       },
-      source: function(query, syncResults, asyncResults) {
+
+      source : function(query, syncResults, asyncResults) {
         API.suggest(query).done(function(results) {
           asyncResults(results);
         }).fail(function(error) {
