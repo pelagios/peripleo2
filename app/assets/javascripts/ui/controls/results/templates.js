e84b22c3aa939d5950fae12eaf4d8970a5629b87
Rainer Simon <rainer.simon@ait.ac.at>
2017-07-11 16:48:44 +0200

    Cleaned up templates.js

--- app/assets/javascripts/ui/controls/results/templates.js
+++ app/assets/javascripts/ui/controls/results/templates.js
@@ -14,51 +14,53 @@ define([
           '</div>' +
         '</li>',
 
-      baseTemplate = function(item) {
+      /** Fills the common basics: title, description, temporal bounds **/
+      renderBase = function(item) {
         var li = jQuery(BASE_TEMPLATE),
             descriptions = ItemUtils.getDescriptions(item);
 
         li.find('.item-title').html(item.title);
-
         if (descriptions.length > 0)
           li.find('.item-description').html(descriptions[0].description);
-
         if (item.temporal_bounds)
           li.find('.item-temporal-bounds').html(Formatting.formatTemporalBounds(item.temporal_bounds));
 
         return li;
       },
 
-      /** Code for rendering the identifer bubbles on places and persons **/
-      appendIdentifiers = function(el, item) {
-        var identifiers = ItemUtils.getURIs(item).map(function(uri) { return ItemUtils.parseEntityURI(uri); }),
+      /** Renders the identifer bubbles on places and persons **/
+      renderIdentifiers = function(el, item) {
+        var identifiers = ItemUtils.getURIs(item).map(function(uri) {
+              return ItemUtils.parseEntityURI(uri);
+            }),
 
-            append = function(ids) {
+            render = function(ids) {
               ids.forEach(function(id) {
-                if (id.isKnownAuthority) {
-                  el.append('<span class="item-id ' + id.shortcode + '" title="' + id.shortcode + ':' + id.id +
-                    '" style="background-color:' + id.color + '">' + id.initial + '</span>');
-                } else {
+                if (id.isKnownAuthority)
+                  el.append(
+                    '<span class="item-id ' + id.shortcode + '" title="' + id.shortcode + ':' + id.id +
+                      '" style="background-color:' + id.color + '">' + id.initial +
+                    '</span>');
+                else
                   el.append('<span class="item-id" title="' + id.uri + '">?</span>');
-                }
               });
             };
 
+        // Append max. 8 bubbles, plus a "more" notification if needed
         if (identifiers.length < 8) {
-          append(identifiers);
+          render(identifiers);
         } else {
-          append(identifiers.slice(1, 8));
+          render(identifiers.slice(1, 8));
           el.append('<span class="item-more-ids">and ' + (identifiers.length - 8) + ' more...</span>');
         }
       },
 
       createObjectRow = function(item) {
-        var li = baseTemplate(item),
+        var li = renderBase(item),
 
             // We'll assume items to have a single record for now
             inDataset = ItemUtils.getHierarchyPath(item.is_conflation_of[0].is_in_dataset);
 
-        // TODO use different icons, depending on item type
         li.addClass('object');
 
         // Only display top-level dataset
@@ -67,31 +69,31 @@ define([
       },
 
       createPlaceRow = function(item) {
-        var li = baseTemplate(item),
+        var li = renderBase(item),
             identifiersEl = jQuery('<p class="item-identifiers-small"></p>').insertAfter(li.find('.item-title'));
 
         li.addClass('place');
-        appendIdentifiers(identifiersEl, item);
+        renderIdentifiers(identifiersEl, item);
         return li;
       },
 
       createPersonRow = function(item) {
-        var li = baseTemplate(item),
+        var li = renderBase(item),
             identifiersEl = jQuery('<p class="item-identifiers-small"></p>').insertAfter(li.find('.item-title'));
 
         li.addClass('person');
-        appendIdentifiers(identifiersEl, item);
+        renderIdentifiers(identifiersEl, item);
         return li;
       },
 
       createPeriodRow = function(item) {
-        var li = baseTemplate(item);
+        var li = renderBase(item);
         li.addClass('period');
         return li;
       },
 
       createDatasetRow = function(item) {
-        var li = baseTemplate(item),
+        var li = renderBase(item),
 
             // We'll assume datasets to have a single record for now
             record = item.is_conflation_of[0],
