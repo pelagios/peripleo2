5a3e7c15194d9991305537c44625994e03836dfa
Rainer Simon <rainer.simon@ait.ac.at>
2017-08-03 15:33:25 +0200

    Maintenance shaft styling

--- app/assets/javascripts/admin/maintenance/app.js
+++ app/assets/javascripts/admin/maintenance/app.js
@@ -7,11 +7,15 @@ require([], function() {
 
   jQuery(document).ready(function() {
 
-    var form = jQuery('#item-id'),
-
+    var form = jQuery('.item-id'),
         input = form.find('input'),
 
-        code = jQuery('#json'),
+        errorMsg = jQuery('.error-message'),
+
+        code = jQuery('.json pre'),
+
+        btnStore = jQuery('button.store'),
+        btnCancel = jQuery('button.cancel'),
 
         fetchItem = function(identifier) {
           jsRoutes.controllers.api.ItemAPIController.getItem(identifier)
@@ -20,14 +24,49 @@ require([], function() {
             });
         },
 
-        onSubmit = function() {
+        onFindByIdentifier = function() {
           var identifier = input.val().trim();
           if (identifier.length > 0)
             fetchItem(identifier);
           return false;
+        },
+
+        onCancel = function() {
+          input.val('');
+          errorMsg.hide();
+          errorMsg.empty();
+          code.empty();
+        },
+
+        onStoreJSON = function() {
+          var json = code.html(),
+
+              // Minimal validation
+              validationError = (function() {
+                try {
+                  JSON.parse(json);
+                  return false;
+                } catch (e) {
+                  return e;
+                }
+              })();
+
+          if (validationError) {
+            errorMsg.html(validationError.message);
+            errorMsg.show();
+
+            // TODO show error message
+          } else {
+            errorMsg.hide();
+            errorMsg.empty();
+
+            // TODO post item back to index
+          }
         };
 
-    form.submit(onSubmit);
+    form.submit(onFindByIdentifier);
+    btnStore.click(onStoreJSON);
+    btnCancel.click(onCancel);
   });
 
 });
