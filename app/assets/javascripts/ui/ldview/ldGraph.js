78b05abb656d26f0dd38babb289f1693967eabf1
Rainer Simon <rainer.simon@ait.ac.at>
2017-08-30 14:05:37 +0200

    Basic D3 force graph integration

--- app/assets/javascripts/ui/ldview/ldGraph.js
+++ app/assets/javascripts/ui/ldview/ldGraph.js
@@ -1,27 +1,53 @@
 define([], function() {
 
-  var DUMMY_GRAPH = {
-    nodes: [
-      { "id": "1" },
-      { "id": "2" },
-      { "id": "3" },
-      { "id": "4" },
-      { "id": "5" }
-    ],
+  var computeGraph = function(item) {
 
-    links: [
-      { "source": "1", "target": "2" },
-      { "source": "1", "target": "3" },
-      { "source": "1", "target": "5" },
-      { "source": "2", "target": "4" },
-      { "source": "4", "target": "5" }
-    ]
-  };
+            /** All links, derived from SKOS close-/exactMatches **/
+        var links = item.is_conflation_of.reduce(function(links, record) {
+              var closeMatches = record.close_matches || [],
+                  exactMatches = record.exact_matches || [],
 
-  // Mostly https://bl.ocks.org/mbostock/4062045
+                  toLinks = function(uris, link) {
+                    return uris.map(function(uri) {
+                      return { source: record.uri, target: uri, link: link };
+                    });
+                  },
+
+                  closeLinks = toLinks(closeMatches, 'closeMatch'),
+                  exactLinks = toLinks(exactMatches, 'exactMatch');
+
+              return links.concat(closeLinks.concat(exactLinks));
+            }, []),
+
+            /** Nodes derived from the records in the item **/
+            knownNodes = item.is_conflation_of.map(function(record) {
+              // TODO extend!
+              return { uri: record.uri };
+            }),
+
+            /** All nodes including 'anonymous' ones based on links to URIs outside of Peripleo **/
+            nodes = links.reduce(function(nodes, link) {
+              var target = link.target,
+
+                  nodeExists = nodes.find(function(node) {
+                    return node.uri === target;
+                  });
+
+              if (!nodeExists)
+                nodes.push({ uri: link.target, is_anonymous: true });
+
+              return nodes;
+            }, knownNodes);
+
+        return { nodes: nodes, links: links };
+      };
+
+  // Mostly http://bl.ocks.org/mbostock/4062045
   var LDGraph = function(d3, svgEl, item) {
 
-    var onDragStart = function(d) {
+    var graph = computeGraph(item),
+
+        onDragStart = function(d) {
           if (!d3.event.active) simulation.alphaTarget(0.3).restart();
           d.fx = d.x;
           d.fy = d.y;
@@ -38,49 +64,57 @@ define([], function() {
           d.fy = null;
         },
 
-        graph = DUMMY_GRAPH, // TODO
-
         svg = d3.select(svgEl),
 
         simulation = d3.forceSimulation()
-          .force("link", d3.forceLink().id(function(d) { return d.id; }))
+          .force('link', d3.forceLink().id(function(d) { return d.uri; }))
           .force('charge', d3.forceManyBody())
           .force('center', d3.forceCenter(100, 100)),
 
-        link = svg.append('g')
-          .attr("class", "links")
+        links = svg.append('g')
           .selectAll('line')
           .data(graph.links)
           .enter()
-            .append('line'),
+            .append('line')
+            .attr('class', function(d) { return d.link; }),
 
-        node = svg.append('g')
-          .attr("class", "nodes")
-          .selectAll('circle')
+        nodes = svg.selectAll('.node')
           .data(graph.nodes)
           .enter()
-            .append('circle')
-            .attr('r', 5)
+            .append('g')
+            .attr('class', 'node')
             .call(d3.drag()
               .on('start', onDragStart)
               .on('drag', onDragged)
               .on('end', onDragEnd)),
 
         ticked = function() {
-          link
+          links
             .attr('x1', function(d) { return d.source.x; })
             .attr('y1', function(d) { return d.source.y; })
             .attr('x2', function(d) { return d.target.x; })
             .attr('y2', function(d) { return d.target.y; });
 
-          node
-            .attr('cx', function(d) { return d.x; })
-            .attr('cy', function(d) { return d.y; });
+          nodes
+            .attr('transform', function(d) { return 'translate(' + d.x + ',' + d.y + ')'; });
         },
 
         stop = function() {
           simulation.stop();
         };
+
+    // Somehow, I hate D3 syntax...
+    nodes.append('circle')
+      .attr('r', 5)
+      .attr('class', function(d) { return (d.is_anonymous) ? 'anonymous' : ''; });
+
+    nodes.append('title')
+      .text(function(d) { return d.uri; });
+
+    nodes.append('text')
+      .attr('dx', 12)
+      .attr('dy', '.35em')
+      .text(function(d) { return d.uri; });
 
     simulation.nodes(graph.nodes).on('tick', ticked);
     simulation.force('link').links(graph.links);
