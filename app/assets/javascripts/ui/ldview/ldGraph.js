5607c7a9060bbb6d823fde9f88e57afb7da60e25
Rainer Simon <rainer.simon@ait.ac.at>
2017-08-30 16:07:33 +0200

    Cleanup

--- app/assets/javascripts/ui/ldview/ldGraph.js
+++ app/assets/javascripts/ui/ldview/ldGraph.js
@@ -65,22 +65,9 @@ define(['ui/common/itemUtils'], function(ItemUtils) {
           d.fy = null;
         },
 
-        svg = d3.select(svgEl[0]);
+        svg = d3.select(svgEl[0]),
 
-    svg.append('svg:defs').selectAll('marker')
-        .data(['end'])
-        .enter().append('marker')
-          .attr('id', String)
-          .attr('viewBox', '0 -5 10 10')
-          .attr('refX', 16)
-          .attr('refY', 0)
-          .attr('markerWidth', 5)
-          .attr('markerHeight', 6)
-          .attr('orient', 'auto')
-          .append('path')
-            .attr('d', 'M0,-5L10,0L0,5');
-
-    var simulation = d3.forceSimulation()
+        simulation = d3.forceSimulation()
           .force('link', d3.forceLink().id(function(d) { return d.uri; }).distance(160))
           .force('charge', d3.forceManyBody().strength(-500))
           .force('center', d3.forceCenter(svgEl.width() / 2, svgEl.height() / 2)),
@@ -121,7 +108,18 @@ define(['ui/common/itemUtils'], function(ItemUtils) {
         };
 
     // Somehow, I hate D3 syntax...
-
+    svg.append('svg:defs').selectAll('marker')
+        .data(['end'])
+        .enter().append('marker')
+          .attr('id', String)
+          .attr('viewBox', '0 -5 10 10')
+          .attr('refX', 16)
+          .attr('refY', 0)
+          .attr('markerWidth', 5)
+          .attr('markerHeight', 6)
+          .attr('orient', 'auto')
+          .append('path')
+            .attr('d', 'M0,-5L10,0L0,5');
 
     nodes.append('circle')
       .attr('r', NODE_RADIUS)
