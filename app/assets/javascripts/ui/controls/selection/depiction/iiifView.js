fe82917e6d7c3a585c46d8493bc6dc007ab0e567
Rainer Simon <rainer.simon@ait.ac.at>
2017-09-10 20:31:41 +0200

    IIIF pane styling

--- app/assets/javascripts/ui/controls/selection/depiction/iiifView.js
+++ app/assets/javascripts/ui/controls/selection/depiction/iiifView.js
@@ -1,12 +1,13 @@
-define([], function() {
+define(['ui/common/formatting'], function(Formatting) {
 
   var IIIFView = function(containerEl, depiction) {
 
-    var iiifPane = L.map(containerEl[0], {
+    var attributionEl = containerEl.find('.attribution'),
+
+        iiifPane = L.map(containerEl[0], {
           center: [0, 0],
           crs: L.CRS.Simple,
           zoom: 0,
-          maxZoom: 10,
           zoomControl: false
         }),
 
@@ -16,6 +17,9 @@ define([], function() {
         destroy = function() {
           containerEl.empty();
         };
+
+    attributionEl.html(Formatting.formatClickableURL(depiction.source));
+    containerEl.prepend('<img class="iiif-logo" src="/assets/images/iiif-logo.png">');
 
     this.destroy = destroy;
   };
