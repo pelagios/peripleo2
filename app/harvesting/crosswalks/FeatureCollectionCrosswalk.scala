4f38e0e3051546fbbf7fd7595eb47a6a0fb88cdd
Rainer Simon <rainer.simon@ait.ac.at>
2017-09-18 10:10:25 +0200

    Generic GeoJSON FeatureCollection gazetteer crosswalk

--- app/harvesting/crosswalks/FeatureCollectionCrosswalk.scala
+++ app/harvesting/crosswalks/FeatureCollectionCrosswalk.scala
@@ -1,16 +1,42 @@
 package harvesting.crosswalks
 
-import java.io.InputStream
-import services.item.ItemRecord
-import play.api.libs.json._
-import services.item.Name
 import com.vividsolutions.jts.geom.Geometry
+import java.io.InputStream
+import org.joda.time.DateTime
+import play.api.libs.json._
+import play.api.libs.json.Reads._
+import play.api.libs.functional.syntax._
+import services.{ HasGeometry, HasNullableSeq }
+import services.item._
 
 object FeatureCollectionCrosswalk {
   
    def fromGeoJSON(filename: String): InputStream => Seq[ItemRecord] = { in =>
      
-     def toItemRecord(json: JsValue): ItemRecord = ???
+     def toItemRecord(json: JsValue): ItemRecord = {
+       val f = Json.fromJson[GazetteerFeature](json).get
+       ItemRecord(
+         f.uri,
+         Seq(f.uri),
+         DateTime.now(),
+         None, // lastChangedAt
+         f.title,
+         None, // TODO isInDataset: Option[PathHierarchy]
+         None, // isPartOf
+         Seq.empty[Category], // TODO get from properties > feature types?
+         Seq.empty[Description],
+         None, // homepage
+         None, // license
+         Seq.empty[Language],
+         Seq.empty[Depiction],
+         f.geometry,
+         f.geometry.map(_.getCentroid.getCoordinate),
+         None, // Temporal bounds TODO support when
+         f.names,
+         f.links.map(_.closeMatches).getOrElse(Seq.empty[String]),
+         f.links.map(_.exactMatches).getOrElse(Seq.empty[String])
+       )
+     }
      
      (Json.parse(in) \ "features") match {
        case JsDefined(JsArray(values)) => values.map(toItemRecord)
@@ -29,11 +55,23 @@ case class GazetteerFeature(
   names    : Seq[Name],
   links    : Option[Links])
   
-object Links {
+object Links extends HasNullableSeq {
+  
+  implicit val linkReads: Reads[Links] = (
+    (JsPath \ "exact_matches").readNullable[Seq[String]].map(fromOptSeq[String]) and
+    (JsPath \ "close_matches").readNullable[Seq[String]].map(fromOptSeq[String])
+  )(Links.apply _)
   
 }
 
-object GazetteerFeature {
+object GazetteerFeature extends HasGeometry with HasNullableSeq {
   
+  implicit val gazetteerFeatureReads: Reads[GazetteerFeature] = (
+    (JsPath \ "uri").read[String] and
+    (JsPath \ "title").read[String] and
+    (JsPath \ "geometry").readNullable[Geometry] and
+    (JsPath \ "names").readNullable[Seq[Name]].map(fromOptSeq[Name]) and
+    (JsPath \ "links").readNullable[Links]
+  )(GazetteerFeature.apply _)
 }
 
--- test/harvesting/crosswalks/FeatureCollectionCrosswalkSpec.scala
+++ test/harvesting/crosswalks/FeatureCollectionCrosswalkSpec.scala
@@ -16,6 +16,14 @@ class FeatureCollectionCrosswalkSpec extends PlaySpec {
       val in = new FileInputStream(SAMPLE)   
       val records = crosswalk(in)
       
+      records.size mustBe 9
+      
+      val sample = records.find(_.uri == "http://www.cyprusgazetteer.org/146853/551")
+      sample.isDefined mustBe true
+      sample.get.exactMatches.size mustBe 0
+      sample.get.closeMatches.size mustBe 1
+      sample.get.closeMatches.head mustBe "http://sws.geonames.org/146853"
+      
       play.api.Logger.info("got " + records.size + " records")
     }
     
