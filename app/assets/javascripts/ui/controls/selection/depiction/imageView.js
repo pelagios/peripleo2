9eac795e3a35325c5c23cdd59135fbed767327a0
Rainer Simon <rainer.simon@ait.ac.at>
2017-10-16 14:56:53 +0200

    Client-side img preload + hook to record broken image links

--- app/assets/javascripts/ui/controls/selection/depiction/imageView.js
+++ app/assets/javascripts/ui/controls/selection/depiction/imageView.js
@@ -4,12 +4,35 @@ define(['ui/common/formatting'], function(Formatting) {
 
     var attributionEl = containerEl.find('.attribution'),
 
+        preloadImage = function(url) {
+          var d = jQuery.Deferred(),
+              img = new Image();
+
+          img.onerror = function() {
+            d.reject(url);
+          };
+
+          img.onload = function() {
+            d.resolve(img);
+          };
+
+          img.src = url;
+          return d.promise();
+        },
+
         destroy = function() {
           containerEl.css('background-image', 'none');
         };
 
-    // TODO pre-load image & report in case of 404
-    containerEl.css('background-image', 'url(' + depiction.url + ')');
+    preloadImage(depiction.url).done(function(img) {
+      containerEl.css('background-image', 'url(' + img.src + ')');
+    }).fail(function(url) {
+      console.log('Image failed: ' + url);
+
+      // TODO notify server-side API
+
+    });
+
     attributionEl.html(Formatting.formatClickableURL(depiction.source));
 
     this.destroy = destroy;
