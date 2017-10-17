6ba9bd17739350cd862ecb713f2240763f6c664e
Rainer Simon <rainer.simon@ait.ac.at>
2017-10-17 14:28:17 +0200

    Form handler + input validation

--- app/controllers/validator/ValidatorController.scala
+++ app/controllers/validator/ValidatorController.scala
@@ -3,6 +3,7 @@ package controllers.validator
 import controllers.WebJarAssets
 import javax.inject.{ Inject, Singleton }
 import play.api.mvc.{ Action, Controller }
+import scala.concurrent.Future
 
 @Singleton
 class ValidatorController @Inject() (
@@ -17,8 +18,27 @@ class ValidatorController @Inject() (
     Ok(views.html.validator.gazetteer())
   }
 
-  def validateGazetteer = Action.async { implicit request =>
-    null // Ok
+  def validateGazetteer = Action.async(parse.multipartFormData) { implicit request =>
+    request.body.file("file").map { filepart =>          
+      request.body.dataParts.get("format").map(_.head) match {
+        case Some(format) =>
+          val file = filepart.ref.file
+          if (file.length > 0) {
+
+            play.api.Logger.info("validating " + filepart.filename + " as " + format)
+            
+            Future.successful(Ok)
+          } else {
+            Future.successful(Redirect(routes.ValidatorController.gazetteer).flashing("error" -> "Please attach a data file"))
+          }
+
+        case None =>
+          Future.successful(Redirect(routes.ValidatorController.gazetteer).flashing("error" -> "Please select a data format"))
+      }      
+    }.getOrElse {
+      // Can never happen, unless someone submits an off-form/hacked request
+      Future.successful(BadRequest)
+    }    
   }
 
   def annotations = Action { implicit request =>
