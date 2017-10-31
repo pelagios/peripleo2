e30da8fd4d6dbb18230540b3d88582be2b26f260
Rainer Simon <rainer.simon@ait.ac.at>
2017-10-31 15:43:22 +0100

    Outlined top search/item/collection over time query

--- app/controllers/admin/analytics/AnalyticsAdminController.scala
+++ app/controllers/admin/analytics/AnalyticsAdminController.scala
@@ -6,15 +6,19 @@ import jp.t2v.lab.play2.auth.AuthElement
 import play.api.Configuration
 import play.api.mvc.Action
 import services.user.{ Role, UserService }
+import services.visit.{ VisitService, TimeInterval }
 
 @Singleton
 class AnalyticsAdminController @Inject() (
   val config: Configuration,
-  val users: UserService,
+  val users:  UserService,
+  val visits: VisitService,
   implicit val webjars: WebJarAssets
 ) extends BaseAuthController with AuthElement {
 
   def index = StackAction(AuthorityKey -> Role.ADMIN) { implicit request =>
+    visits.countTotalSince(TimeInterval.LAST_24HRS)
+    visits.countTotalSince(TimeInterval.LAST_7DAYS)
     Ok(views.html.admin.analytics.index())
   }
 
--- app/services/visit/VisitService.scala
+++ app/services/visit/VisitService.scala
@@ -34,5 +34,44 @@ class VisitService @Inject() (val es: ES, implicit val ctx: ExecutionContext) {
     es.client execute {
       search in ES.PERIPLEO / ES.VISIT limit 0
     } map { _.totalHits }
+    
+  def countTotalSince(since: TimeInterval) = {
+    
+    import TimeInterval._
+    
+    val expression = since match {
+      case LAST_24HRS => "now-24h"
+      case LAST_7DAYS => "now-7d"
+    }
+    
+    es.client execute {
+      search in ES.PERIPLEO / ES.VISIT query {
+        constantScoreQuery {
+          filter ( rangeQuery("visited_at") from expression )
+        }
+      } size 0 aggregations (
+        aggregation terms "top_items"    field "selection.identifier" size 10,
+        aggregation terms "top_datasets" field "selection.is_in_dataset.ids" size 10,
+        aggregation terms "top_searches" field "search.query.raw" size 10 
+      )
+    } map { response =>
+      
+      play.api.Logger.info(response.toString)
+      
+    }
+    
+  }
+  
+}
+
+sealed trait TimeInterval
+
+object TimeInterval {
+  
+  case object TODAY      extends TimeInterval
+  case object LAST_24HRS extends TimeInterval
+  case object LAST_7DAYS extends TimeInterval
+  case object THIS_WEEK  extends TimeInterval
+  case object THIS_MONTH extends TimeInterval
   
 }
