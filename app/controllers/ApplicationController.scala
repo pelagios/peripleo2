c7204775602a0a8b05b7e6a0cfcf3705434c38dd
Rainer Simon <rainer.simon@ait.ac.at>
2017-11-13 13:14:38 +0100

    Added 'top item' embed to splash page

--- app/controllers/ApplicationController.scala
+++ app/controllers/ApplicationController.scala
@@ -1,16 +1,18 @@
 package controllers
 
-import javax.inject.{ Inject, Singleton }
-import play.api.mvc.{ Action, Controller }
+import javax.inject.{Inject, Singleton}
+import play.api.mvc.{Action, Controller}
 import scala.concurrent.ExecutionContext
-import services.item.{ ItemService, ItemType }
+import services.item.{ItemService, ItemType}
 import services.item.search._
+import services.visit.{VisitService, TimeInterval} 
 import org.joda.time.Period
 
 @Singleton
 class ApplicationController @Inject() (
   val itemService: ItemService,
   val searchService: SearchService,
+  val visitService: VisitService,
   implicit val ctx: ExecutionContext,
   implicit val webjars: WebJarAssets
 ) extends Controller {
@@ -20,14 +22,17 @@ class ApplicationController @Inject() (
       searchService.getTimerange(SearchArgs(None, 0, 0, SearchFilters.NO_FILTERS, ResponseSettings.DEFAULT))
     val fDatasets =
       itemService.findByType(ItemType.DATASET, true, 0, 0)
-          
+    val fVisitStats= 
+      visitService.getStatsSince(TimeInterval.LAST_7DAYS)
+    
     val f = for {
       t <- fTimerange
       datasets <- fDatasets
-    } yield (t.totalHits, datasets.total, new Period(t.from, t.to))
+      visitStats <- fVisitStats
+    } yield (t.totalHits, datasets.total, new Period(t.from, t.to), visitStats.topItems.headOption.map(_._1.identifier))
     
-    f.map { case (itemCount, datasetCount, timerange) =>
-      Ok(views.html.landing.index(itemCount, datasetCount, timerange.getYears))
+    f.map { case (itemCount, datasetCount, timerange, topItemId) =>
+      Ok(views.html.landing.index(itemCount, datasetCount, timerange.getYears, topItemId))
     }
   }
   
